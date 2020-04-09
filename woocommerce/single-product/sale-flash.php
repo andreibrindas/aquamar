97bf61364dfaf671b57b3fe909c58d0786aef0f5
andreibrindas <31765195+andreibrindas@users.noreply.github.com>
2020-04-09 09:29:26 +0300

    changes

--- woocommerce/single-product/sale-flash.php
+++ woocommerce/single-product/sale-flash.php
@@ -24,17 +24,9 @@ global $post, $product;
 ?>
 <?php if ( $product->is_on_sale() ) : ?>
 
-	<?php
-		
-		$new_price = $product->price;
-		$regular_price = $product->regular_price;
-		// echo $new_price;
 
-		$price = '-' . round( 100 - ( $new_price / $regular_price * 100 ), 1 ) . '%';
-		
-	?>	
 
-	<?php echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale">' . esc_html__( $price, 'woocommerce' ) . '</span>', $post, $product ); ?>
+	<?php echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale">' . esc_html__( '-', 'woocommerce' ) . '</span>', $post, $product ); ?>
 
 	<?php
 endif;
